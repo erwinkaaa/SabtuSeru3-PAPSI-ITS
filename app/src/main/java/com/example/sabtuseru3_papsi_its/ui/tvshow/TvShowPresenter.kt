9abd5e0535c3babd0e7f5bda68c81ae4e986abd3
Erwin Kurniawan Adidharma <erwinkurniawanadi@gmail.com>
2020-03-02 12:01:01 +0700

    add helper, change presenter

--- app/src/main/java/com/example/sabtuseru3_papsi_its/ui/tvshow/TvShowPresenter.kt
+++ app/src/main/java/com/example/sabtuseru3_papsi_its/ui/tvshow/TvShowPresenter.kt
@@ -1,5 +1,6 @@
 package com.example.sabtuseru3_papsi_its.ui.tvshow
 
+import com.example.sabtuseru3_papsi_its.helper.CoroutineContextProvider
 import com.example.sabtuseru3_papsi_its.repository.ApiRepository
 import com.example.sabtuseru3_papsi_its.repository.TheMovieDBAPI
 import com.google.gson.Gson
@@ -10,13 +11,14 @@ import kotlinx.coroutines.launch
 class TvShowPresenter(
     private val view: TvShowView,
     private val gson: Gson,
-    private val apiRepository: ApiRepository
+    private val apiRepository: ApiRepository,
+    private val contextPool: CoroutineContextProvider = CoroutineContextProvider()
 ) {
 
     fun loadTvShows() {
         view.showLoading()
 
-        GlobalScope.launch(Dispatchers.Main) {
+        GlobalScope.launch(contextPool.main) {
             val data = gson.fromJson(
                 apiRepository.doRequestAsync(TheMovieDBAPI.discoverTvShowUrl()).await(),
                 TVShowResponseModel::class.java
