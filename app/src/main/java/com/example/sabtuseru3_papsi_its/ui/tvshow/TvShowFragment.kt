c6182d73be8d3446867437786454b80bda31ceb0
Erwin Kurniawan Adidharma <erwinkurniawanadi@gmail.com>
2020-03-13 16:25:52 +0700

    update starter

--- app/src/main/java/com/example/sabtuseru3_papsi_its/ui/tvshow/TvShowFragment.kt
+++ app/src/main/java/com/example/sabtuseru3_papsi_its/ui/tvshow/TvShowFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.sabtuseru3_papsi_its.R
+import com.example.sabtuseru3_papsi_its.ui.detail_tv.DetailTvActivity
+import kotlinx.android.synthetic.main.fragment_tv_show.*
+import org.jetbrains.anko.support.v4.startActivity
 
 /**
  * A simple [Fragment] subclass.
  */
 class TvShowFragment : Fragment() {
+
+    private lateinit var adapter: TvShowAdapter
+    private var tvShows = mutableListOf<TvShowModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +26,18 @@ class TvShowFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tv_show, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        adapter = TvShowAdapter(tvShows) {
+            startActivity<DetailTvActivity>(DetailTvActivity.TV_ID to it.id)
+        }
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
     }
 
 }
